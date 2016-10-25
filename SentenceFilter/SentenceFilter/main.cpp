24b435b601b9186d73575391c3b34bc50380c03e
Shane Doulen <shane@Shanes-MacBook-Pro.local>
2016-10-25 12:11:04 -0400

    First commit

---
+++ SentenceFilter/SentenceFilter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SentenceFilter
+//
+//  Created by Shane Doulen on 10/25/16.
+//  Copyright © 2016 Shane Doulen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
